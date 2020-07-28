57905239e8543843032258aae63ebd3fa57827a2
shadidintisar <shadidintisar@gmail.com>
2020-07-28 23:06:31 +0600

    #15 1st unit test written.Prepared Test Framework

---
+++ Age_of_villagers/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Age_of_villagers
+{
+    public class UnitTest
+    {
+
+        public void Test()
+        {
+            int a = 10;
+            int b = 20;
+            var c = a + b;
+          
+            Assert.Equals(30, c);
+        }
+    }
+}
